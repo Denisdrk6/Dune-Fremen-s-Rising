d37b22b0c889dcb0ed1ade567f8d02dec157970f
adrianam4 <adrian.aroca.joliver@gmail.com>
2022-05-22 19:16:55 +0200

    Cinematic 3

--- Output/Assets/Scripts/mainMenuBackScreen.cs
+++ Output/Assets/Scripts/mainMenuBackScreen.cs
@@ -487,7 +487,7 @@ public class mainMenuBackScreen : RagnarComponent
                     SceneAudio.GetComponent<AudioSource>().PlayClip("UI_SELECT");
 
 					SaveSystem.fromContinue = false;
-                    SceneManager.LoadScene("Cinematic_7");
+                    SceneManager.LoadScene("Cinematic_3");
                     //cambiar de escena
                     break;
             }
